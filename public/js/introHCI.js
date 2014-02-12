a4437bbe07ca703144a96573fc495c71eb96bbd0
vagrant <vagrant@precise32.(none)>
2014-02-12 23:34:39 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -21,12 +21,40 @@ function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
 
+	//$('#wait').show();
 	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/" + idNumber, addProject);
+
+}
+
+function addProject(result) {
+	console.log(result);
+	//$('wait').hide();
+
+	var projectHTML = '<img src="' + result['image'] + '" class="detailsImage">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +
+    '</small></p>' + '<p>' + result['summary'] + '</p>'; 
+	console.log($("#project" + result.id + " .details "));
+	$("#project" + result.id + " .details ").html(projectHTML);
+	console.log(result);
+}
+
+function addPalette(result){
+	console.log(result);
+
+	var colors = result.colors.hex;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
 
 /*
@@ -34,5 +62,6 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
+	$.get("/palette", addPalette);
 	console.log("User clicked on color button");
 }
